d5f547c90ad5cf5d2aeac0089443363dbf213cb5
Farooq <farooqhmf@hotmail.com>
2019-04-28 15:44:31 +0500

    commitng to changes

---
+++ src/container/Home/index.js
@@ -0,0 +1,39 @@
+
+import React, { Component } from 'react';
+import './styles.css';
+
+class Home extends Component {
+
+  count = 0;
+  handleClick(data){
+    console.log('hello', this.count);
+    console.log(data);
+  }
+  handleunboundClick(){
+    this === 'undefind' ? console.log('this is undefind') : console.log('value', this);
+  }
+    render(){
+        return (
+            <div>
+                <h1>Home</h1>
+                <button onClick={() => this.handleClick('data')}>Click me</button>
+                <button onClick={ this.handleunboundClick}>get count</button>
+                <div id="text-align" className="blue-div">
+                    <div className="blue-div-child">
+                      <p>Blue</p>
+                      <p>BlueBlueBlueBlue</p>
+                      <p>BlueBlue</p>
+                    </div>
+                </div>
+                <div id="margin-auto" className="yellow-div">
+                    <div className="yellow-div-child"></div>
+                </div>
+                <div id="absolute-position" className="green-div">
+                    <div className="green-div-child"></div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Home;
